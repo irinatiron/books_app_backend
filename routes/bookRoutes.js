d349fbb12c52c0eb81cc9950c950f7691ea823fe
irinatiron <irinatirongiurgiu@gmail.com>
2025-10-08 13:13:02 +0200

    feat(Middleware): checkRole on bookRoutes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { getAllBooks, getOneBook, createBook, updateBook, deleteBook } from '../controllers/BookController.js'
 import { authMiddleware } from '../middlewares/AuthMiddleware.js'
+import { checkRole } from '../middlewares/RoleMiddleware.js'
 const bookRouter = express.Router()
 bookRouter.get('/', getAllBooks)
 bookRouter.get('/:id', getOneBook)
-bookRouter.post('/', authMiddleware, createBook)
+bookRouter.post('/', authMiddleware, checkRole(['admin']), createBook)
 bookRouter.delete('/:id', deleteBook)
 bookRouter.put('/:id', updateBook)
 export default bookRouter
